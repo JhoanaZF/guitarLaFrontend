474cfec8673d3416c4313724d08f98e24fa57791
jhoana <jhoanazamora2@gmail.com>
2024-01-18 00:44:23 -0500

    Añadiendo CSS al Footer

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -5,6 +5,9 @@ const Footer = () => {
     <footer className='footer'>
       <div className='contenedor contenido'>
         <Navegacion />
+        <p className='copyright'>
+          Todos los derechos reservados {new Date().getFullYear()}
+        </p>
       </div>
     </footer>
   );
